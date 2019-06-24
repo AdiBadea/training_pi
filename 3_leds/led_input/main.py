fa226a5ea8a42cfa48f1c0e9160ad44e10f35285
Roberta Sirbu <badeaadrianionut@gmail.com>
2019-06-24 22:56:11 +0300

    Lucrez la functia de pus intrebari

--- 3_leds/led_input/main.py
+++ 3_leds/led_input/main.py
@@ -1,10 +1,31 @@
-print("Ce bec doresti sa aprinzi?")
-print("1 - Verde | 2 - Galben | 3 - Rosu")
+# print("1 - Verde | 2 - Galben | 3 - Rosu")
 
-print("Pentru cate secunde ?")
-secundeAprins = input()
+# print("Pentru cate secunde ?")
+# secundeAprins = input()
 
-aprinsBec = input()
-print("Aprindem " + aprinsBec)
-print("Pentru ", secundeAprins, "secunde")
+# aprinsBec = input()
+# print("Aprindem " + aprinsBec)
+# print("Pentru ", secundeAprins, "secunde")
+
+parametrii = []
+
+class AprindeBec:
+
+    def puneIntrebari():
+        print("Ce bec doresti sa aprinzi?")
+        print("1 - Verde | 2 - Galben | 3 - Rosu")
+        becDeAprins = input()
+        print("Aprindem becul ", str(becDeAprins))
+
+        return becDeAprins
+
+    print(puneIntrebari())
+
+
+AprindeBec()
+
+
+
+
+    
 
