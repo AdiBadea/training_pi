83d80accbaef0b65dcdfef827698638d31e2032e
Roberta Sirbu <badeaadrianionut@gmail.com>
2019-06-24 22:18:20 +0300

    Lucrez la led input

---
+++ 3_leds/led_input/main.py
@@ -0,0 +1,10 @@
+print("Ce bec doresti sa aprinzi?")
+print("1 - Verde | 2 - Galben | 3 - Rosu")
+
+print("Pentru cate secunde ?")
+secundeAprins = input()
+
+aprinsBec = input()
+print("Aprindem " + aprinsBec)
+print("Pentru ", secundeAprins, "secunde")
+
