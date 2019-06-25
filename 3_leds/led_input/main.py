dbb36d9bf1c608bd26be47ef3515cc6e7bc6a340
AdiBadea <badeaadrianionut@gmail.com>
2019-06-25 16:30:34 +0300

    M-am jucat cu erori

--- 3_leds/led_input/main.py
+++ 3_leds/led_input/main.py
@@ -1,28 +1,53 @@
-# print("1 - Verde | 2 - Galben | 3 - Rosu")
+# import only system from os 
+from os import system, name 
+# import sleep to show output for some time period 
+from time import sleep 
 
-# print("Pentru cate secunde ?")
-# secundeAprins = input()
+numereLeduri = ["1", "2", "3"]
 
-# aprinsBec = input()
-# print("Aprindem " + aprinsBec)
-# print("Pentru ", secundeAprins, "secunde")
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
 
-parametrii = []
-
-class AprindeBec:
-
-    def puneIntrebari():
+def intrabaLed():
         print("Ce bec doresti sa aprinzi?")
         print("1 - Verde | 2 - Galben | 3 - Rosu")
         becDeAprins = input()
-        print("Aprindem becul ", str(becDeAprins))
+        print(type(becDeAprins))
+        if becDeAprins in numereLeduri:
+            clear() 
+            return becDeAprins
+        else:
+            print("Bec invalid, becurile disponibile sunt 1, 2 sau 3")
+            return "eroare"
+           
 
-        return becDeAprins
+def intrabaSecunde():
+        print("Pentru cate secunde ?")
+        secundeDeAprins = input()
+        clear() 
+        return str(secundeDeAprins)        
 
-    print(puneIntrebari())
-
+class AprindeBec:
+    def __init__(self):
+        if intrabaLed() != "eroare":
+            print(intrabaLed())
+            print(intrabaSecunde())
 
 AprindeBec()
+
+
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
 
 
 
