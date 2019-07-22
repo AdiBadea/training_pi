e62d1d83a114b56684f41bdd056727e913f49ce4
AdiBadea <badeaadrianionut@gmail.com>
2019-07-22 22:03:38 +0300

    Am creeat array ul cu date

--- opticalmed_extrapolation/extrapolation.py
+++ opticalmed_extrapolation/extrapolation.py
@@ -1,13 +1,20 @@
 import csv
 
-with open('C:/Users/AdrianBadea/Projects/training_pi/opticalmed_extrapolation/test.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+data = []
+
+with open('C:/Users/AdrianBadea/Projects/training_pi/opticalmed_extrapolation/data.csv') as csvFile:
+    rawData = csv.reader(csvFile, delimiter=',')
+    rowCount = 0
+    for row in rawData:
+        # print(row[0] + " - " + row[1])
+        data.append([row[0], row[1]])
+        rowCount += 1
+    print(data) 
+
+    #     if lineCount == 0:
+    #         print(f'Column names are {", ".join(row)}')
+    #         line_count += 1
+    #     else:
+    #         print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+    #         line_count += 1
+    # print(f'Processed {line_count} lines.')
