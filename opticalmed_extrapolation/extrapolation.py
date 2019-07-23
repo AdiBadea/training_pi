e7e7b259f17a2a592c9258da1617fbea2e81a41c
AdiBadea <badeaadrianionut@gmail.com>
2019-07-23 20:16:31 +0300

    Am creeat o functie care imi da 2 array-uri diferite

--- opticalmed_extrapolation/extrapolation.py
+++ opticalmed_extrapolation/extrapolation.py
@@ -6,18 +6,17 @@ income = []
 dates = []
 
 with open('C:/Users/AdrianBadea/Projects/training_pi/opticalmed_extrapolation/data.csv') as csvFile:
+    
     rawData = csv.reader(csvFile, delimiter=',')
     rowCount = 0
+
     for row in rawData:
-        # print(row[0] + " - " + row[1])
         data.append([row[0], row[1]])
         rowCount += 1
-    print(data) 
+    del data[0]
 
-    #     if lineCount == 0:
-    #         print(f'Column names are {", ".join(row)}')
-    #         line_count += 1
-    #     else:
-    #         print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-    #         line_count += 1
-    # print(f'Processed {line_count} lines.')
+    for row in data:
+        dates.append(row[0])
+        income.append(row[1])
+    print(income)
+    print(dates)
