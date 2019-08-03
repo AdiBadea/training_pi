d1cc9502e9ca838cacf609c832dedc83c50a9916
AdiBadea <badeaadrianionut@gmail.com>
2019-08-03 13:20:16 +0300

    am modificat pinii â

--- motors/startMotor.py
+++ motors/startMotor.py
@@ -8,19 +8,17 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 
-
 for repetitions in range(0, 5):
     
-    GPIO.output(7, True)
+    GPIO.output(13, True)
     GPIO.output(11, True)
-
   
     # Pornim pin-ul
     
     time.sleep(3)
     # Stam o secunda
     
-    GPIO.output(7, False)
+    GPIO.output(13, False)
     GPIO.output(11, False)
     # Oprim pin-ul
     
