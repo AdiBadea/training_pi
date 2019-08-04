21296f228091d000e658298df406fbaad04e2cbb
AdiBadea <badeaadrianionut@gmail.com>
2019-08-04 07:54:27 +0300

    Fac totul sa mearga in mod OO

--- opticalmed_extrapolation/classes/readData.py
+++ opticalmed_extrapolation/classes/readData.py
@@ -3,14 +3,26 @@ import csv
 
 class ReadData():
     def __init__(self, filePath):
-        # setting variables
-        # self.filePath = filePath
 
         self.openReadFile(filePath)
 
     def openReadFile(self, filePath):
-        # self.filePath = filePath
-        print(filePath)
 
-ReadData("test/test")
+        data = []
+
+        with open(filePath) as csvFile:
+
+
+            rawData = csv.reader(csvFile, delimiter=',')
+            rowCount = 0
+
+            for row in rawData:
+                data.append([row[0], row[1]])
+                rowCount += 1
+                del data[0]
+
+            print(filePath)
+            print(data)
+
+ReadData('C:/Proiecte/training_pi/opticalmed_extrapolation/data.csv')
 
