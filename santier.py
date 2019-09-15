8bdcda16a867635cf341b4266934acd9221ce863
AdiBadea <badeaadrianionut@gmail.com>
2019-09-15 11:43:03 +0300

    Am adaugat notite noi in jurnal

--- santier.py
+++ santier.py
@@ -9,21 +9,21 @@ GPIO.setmode(GPIO.BOARD)
 
 # Setam ca pin-ul 7 sa emita curent
 GPIO.setup(7, GPIO.OUT)
-GPIO.setup(8, GPIO.OUT)
-GPIO.setup(10, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
 for repetitions in range(0, 10):
     GPIO.output(7, True)
-    GPIO.output(8, True)
-    GPIO.output(10, True)
     GPIO.output(11, True)
+    GPIO.output(13, True)
+    GPIO.output(15, True)
     time.sleep(1)
 
     GPIO.output(7, False)
-    GPIO.output(8, False)
-    GPIO.output(10, False)
     GPIO.output(11, False)
+    GPIO.output(13, False)
+    GPIO.output(15, False)
     time.sleep(1)
     
 GPIO.cleanup()
