7a15f4792f1a9ca803fdf74131f328bfdcfedb80
AdiBadea <badeaadrianionut@gmail.com>
2019-09-15 10:52:44 +0300

    Am mai adaugat un bec

--- santier.py
+++ santier.py
@@ -9,13 +9,16 @@ GPIO.setmode(GPIO.BOARD)
 
 # Setam ca pin-ul 7 sa emita curent
 GPIO.setup(7, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
 
 
 for repetitions in range(0, 10):
     
     GPIO.output(7, True)
+    GPIO.output(8, True)
     time.sleep(1)
     GPIO.output(7, False)
+    GPIO.output(8, False)
     time.sleep(1)
 
     
