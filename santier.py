3ec89f00db31988e6927f918238f576d14519341
AdiBadea <badeaadrianionut@gmail.com>
2019-09-15 10:34:13 +0300

    am trecut de pe pinul 3 pe 7 ca in exemplu

--- santier.py
+++ santier.py
@@ -1,20 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(false)
+GPIO.setwarnings(False)
 
 # Setam modul de numerotare al pinilor
 GPIO.setmode(GPIO.BOARD)
 
 # Setam ca pin-ul 7 sa emita curent
-GPIO.setup(3, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
 
 
 for repetitions in range(0, 10):
     
-    GPIO.output(3, True)
+    GPIO.output(7, True)
     time.sleep(2)
-    GPIO.output(3, False)
+    GPIO.output(7, False)
  
     
 GPIO.cleanup()
