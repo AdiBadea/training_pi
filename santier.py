5d823126b0e32d5a126acfa998f086953efc3de5
AdiBadea <badeaadrianionut@gmail.com>
2019-09-15 10:22:07 +0300

    am creat un program scurt sa testez un bec

---
+++ santier.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+# Setam modul de numerotare al pinilor
+GPIO.setmode(GPIO.BOARD)
+
+# Setam ca pin-ul 7 sa emita curent
+GPIO.setup(3, GPIO.OUT)
+
+
+for repetitions in range(0, 10):
+    
+    GPIO.output(3, True)
+    time.sleep(2)
+    GPIO.output(3, False)
+ 
+    
+GPIO.cleanup()
