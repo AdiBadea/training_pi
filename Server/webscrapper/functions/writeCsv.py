e4478cd2d43692de7c8d86802d766abd2fddc911
AdiBadea <badeaadrianionut@gmail.com>
2019-09-16 15:55:00 +0300

    Am finisat putin lucruurle, am programat ca fisierul de out safie salvat in fisierul output

--- Server/webscrapper/functions/writeCsv.py
+++ Server/webscrapper/functions/writeCsv.py
@@ -1,9 +1,14 @@
 import csv
+import os.path
+
+outputFolder = "C:/Users/AdrianBadea/Projects/training_pi/Server/webscrapper/webscrappers/ouput"
+fileName = "webscrape_history.csv"
+completeName = os.path.join(outputFolder, fileName)
 
 def writeCsv(input):
     print(input)
-    with open("webscrape_history.csv", mode="a+", newline='') as csv_file:
+    with open(completeName, mode="a+", newline='') as csv_file:
         fieldnames = ["Data", "Comerciant", "Produs", "Pret"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
+        # writer.writeheader()
         writer.writerow({"Data": input["scrapeDate"], "Comerciant": input["seller"], "Produs": input["title"], "Pret": input["price"]})
