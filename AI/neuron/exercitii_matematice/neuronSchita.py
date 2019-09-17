1708e5b5b3dc5e93714acfff0b559b180f131087
AdiBadea <badeaadrianionut@gmail.com>
2019-09-17 09:34:12 +0300

    Aam codat neuronul meu frumos foc

--- AI/neuron/exercitii_matematice/neuronSchita.py
+++ AI/neuron/exercitii_matematice/neuronSchita.py
@@ -8,3 +8,9 @@ i2 = 1
 neuronOut = ((w1 * i1) + (w2 * i2)) + b
 
 print(neuronOut)
+
+def sigmoid(x):
+# Functia de activare: f(x) = 1 / (1 + e^(-x))
+return 1 / (1 + np.exp(-x))
+
+print(sigmoid(neuronOut))
--- AI/neuron/neuron.py
+++ AI/neuron/neuron.py
@@ -6,13 +6,26 @@ def sigmoid(x):
     # e = 2.7182
     # np.exp(-x) = e * e, de x ori
 
-# print(sigmoid(3))
-
-def class Neuron:
+class Neuron:
     def __init__(self, weights, bias):
         self.weights = weights
         self.bias = bias
 
     # Feedforward = Aa introduce input-urile in neuron
     def feedforward(self, inputs):
+        result = np.dot(weights, inputs) + bias
+        return sigmoid(result)
+
+# ------------------------------------------------------
+
+weights = np.array([1,1])
+bias = 4
+
+neuron = Neuron(weights, bias)
+
+inputs = np.array([1, 1])
+
+neuronCalculation = neuron.feedforward(inputs)
+
+print(neuronCalculation)
 
