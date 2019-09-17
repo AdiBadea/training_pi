1bba8d6f773b8b0b028a34f08ce0d931e1480292
AdiBadea <badeaadrianionut@gmail.com>
2019-09-17 09:27:43 +0300

    Am invatat ce fce dot product

--- AI/neuron/neuron.py
+++ AI/neuron/neuron.py
@@ -6,4 +6,13 @@ def sigmoid(x):
     # e = 2.7182
     # np.exp(-x) = e * e, de x ori
 
+# print(sigmoid(3))
+
+def class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+
+    # Feedforward = Aa introduce input-urile in neuron
+    def feedforward(self, inputs):
 
---
+++ AI/neuron/numpyDot.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+arr1 = [2, 2]
+arr2 = [3, 4]
+
+print(np.dot(arr1, arr2))
+# np.dot(a, b) = (a1 * b1) + (a2 * b2) + ... + (an * bn) 
